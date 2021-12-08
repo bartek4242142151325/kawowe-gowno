867e417c813f0821ca4ea59cbb374541f358e99e
bartek <bartlomiej.fecko@gmail.com>
2021-12-08 21:35:30 +0100

    init

---
+++ src/main/java/com/bfecko/ApplicationEntry.java
@@ -0,0 +1,21 @@
+package com.bfecko;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/rest")
+public class ApplicationEntry extends Application {
+
+    private Set<Object> singletons = new HashSet<>();
+
+    public ApplicationEntry() {
+        singletons.add(new UserService());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+}
---
+++ src/main/java/com/bfecko/UserService.java
@@ -0,0 +1,18 @@
+package com.bfecko;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+public class UserService {
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getAll() {
+        return Response.status(Response.Status.OK).entity("yep").build();
+    }
+}
